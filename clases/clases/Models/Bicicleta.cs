bd29cd23798fd7f300984d5820256ee2cef891d3
aguisergio <40550432+aguisergio@users.noreply.github.com>
2018-06-24 21:41:16 -0400

    Add files via upload

---
+++ clases/clases/Models/Bicicleta.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace clases.Models
+
+{
+    public class Bicicleta
+    {
+        public int Id {get; set;}
+        public string Serial {get; set;}
+        public string Color {get; set;}
+        public string Rin {get; set;}
+        public DateTime Fecha_compra {get; set;}
+
+    }
+}
